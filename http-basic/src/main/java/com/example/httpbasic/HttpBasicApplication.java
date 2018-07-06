ede6baa83b4739967c6e43a2b27e01c47f4b120e
Mario Gray <mario.gray@gmail.com>
2018-07-05 18:03:12 -0700

    adds http-basic, oauth2 example setup

---
+++ http-basic/src/main/java/com/example/httpbasic/HttpBasicApplication.java
@@ -0,0 +1,12 @@
+package com.example.httpbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpBasicApplication.class, args);
+	}
+}
